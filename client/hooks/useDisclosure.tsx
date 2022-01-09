28a37f818f7bb68aade354e6cf98ddf00fb9f714
Ren0503 <illuminati0440@gmail.com>
2022-01-09 10:18:04 +0700

    Add hooks

---
+++ client/hooks/useDisclosure.tsx
@@ -0,0 +1,16 @@
+import { useCallback, useState } from 'react';
+
+function useDisclosure(onCloseExtension?: () => void) {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const onOpen = useCallback(() => setIsOpen(true), []);
+    const onClose = useCallback(() => {
+        setIsOpen(false);
+        if (onCloseExtension) onCloseExtension();
+    }, []);
+    const toggle = () => setIsOpen(!isOpen);
+
+    return { isOpen, onOpen, onClose, toggle };
+}
+
+export default useDisclosure;
